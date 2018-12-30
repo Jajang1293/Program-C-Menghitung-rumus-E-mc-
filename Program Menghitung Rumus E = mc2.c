d3e8b6ef32b7c23f60c505a373e9ab07893b0874
Jajang Jaelani <45264588+Jajang1293@users.noreply.github.com>
2018-12-30 21:33:44 +0700

    Add files via upload

---
+++ Program Menghitung Rumus E = mc2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+void main()
+{
+    float c=89.88,m,E;
+    printf("Program Perhitungan Menggunakan Rumus E = mc2\n");
+    printf("-------------------------------------------------\n");
+    printf("Masukkan massa (Kg) : ");scanf("%f", &m);
+    E=m*c*c;
+    printf("E = %.2f*%.2f*%.2f = %.2f J",m,c,c,E);
+}
